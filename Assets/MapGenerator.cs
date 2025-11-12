ca25a2455fae86356d7a5c891b46f2b92bcfb4af
Shawn Bernard <ssbernard@outlook.com>
2025-11-11 22:40:25 -0400

    Added map gen logic

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -1,96 +1,194 @@
-using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime.Tree;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEditor;
 
 public class MapGenerator : MonoBehaviour
 {
-    private int[,] map;
+    [Header("City Settings")]
+    [Range(30,100)]
+    public int width = 50;
+    [Range(30, 100)]
+    public int height = 50;
+    private int roadWidth = 2;
+    [Range(10, 20)]
+    public int roadInterval = 10;
+    [Header("Prefabs")]
+    public GameObject cube;
+    private int[,] map; // 0 = road 1 = building plot
 
-    public int width;
-    public int height;
+    private int road 
+    {  
+        get => 0;
+    }
+    private int building
+    {
+        get => 1;
+    }
 
-    List<intersection> intersections;
-    List<road> roads;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        map = new int[width,height];
-        intersections = new List<intersection>();
-        roads = new List<road>();
-
-        int rngX = Random.Range(0, width);
-        int rngY = Random.Range(0, height);
-
-        for (int x = 0; x < map.GetLength(0); x++)
-        {
-            for (int y = 0; y < map.GetLength(1); y++)
-            {
-                if (x == rngX && y == rngY)
-                {
-                    Debug.Log("Intersection found");
-                    intersection section = new intersection(x, y);
-                    intersections.Add(section);
-                }
-                
-            }
-        }
+        GenerateCity();
     }
 
-    // Update is called once per frame
     void Update()
     {
-    }
-
-    bool InMapRange(int x,int y)
-    {
-        return x >= 0 && x < width && y >= 0 && y < height;
-    }
-
-    class road
-    {
-        int startPosition;
-        int endPosition;
-
-        int rX;
-        int rY;
-        public road(int x,int y)
+        if (Keyboard.current[Key.Space].wasPressedThisFrame)
         {
-            rX = x;
-            rY = y;
+            GenerateCity();
         }
     }
 
-    class intersection
+    public void GenerateCity()
     {
-        public int posX;
-        public int posY;
+        DestroyCity();
+        map = new int[width, height];
 
-        public road topRoad, leftRoad, bottomRoad, rightRoad;
+        FillMap();
 
-        public intersection(int _posX,int _posY)
-        {
-            posX = _posX;
-            posY = _posY;
+        CreateGrid();
 
-            topRoad = new road(posX, posY + 1);
-            leftRoad = new road(posX - 1, posY);
-            bottomRoad = new road(posX, posY - 1);
-            rightRoad = new road(posX + 1, posY);
-
-        }
+        AddCityObjects();
+        
     }
 
-    private void OnDrawGizmos()
+    void AddCityObjects()
     {
-        if (Application.isPlaying && map != null)
+        for (int x = 0; x < width; x++)
         {
-            if (intersections != null)
-            foreach (var item in intersections)
+            for (int y = 0; y < height; y++)
             {
-                Gizmos.color = Color.red;
-                var position = new Vector3(item.posX, 0, item.posY);
-                Gizmos.DrawCube(position,Vector3.one * 5);
+                if (map[x, y] == road)
+                {
+                    CreateRoad(x, y);
+                }
+                else if (map[x, y] == building)
+                {
+                    CreateBuilding(x, y);
+                }
             }
         }
+    }
+    /// <summary>
+    /// Places the border road and fills the rest with buildings
+    /// </summary>
+    void FillMap()
+    {
+        int halfRoadWidth = roadWidth / 2;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                // Making my borders roads
+                if (x == halfRoadWidth || y == halfRoadWidth || x == (width - halfRoadWidth) || y == (height - halfRoadWidth))
+                {
+                    MarkRoad(x, y);
+                }
+                else
+                {
+                    map[x, y] = building;
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// Marks roads at each road interval for horizontal and vertical
+    /// </summary>
+    void CreateGrid()
+    {
+        for (int x = 0; x < width; x += roadInterval)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                MarkRoad(x, y);
+            }
+        }
+        for (int y = 0; y < height; y += roadInterval)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                MarkRoad(x, y);
+            }
+        }
+    }
+    /// <summary>
+    /// Takes in the position and marks nearby position as road to be wider
+    /// </summary>
+    /// <param name="roadX"></param>
+    /// <param name="roadY"></param>
+    void MarkRoad(int roadX, int roadY)
+    {
+        int half = Mathf.CeilToInt(roadWidth / 2);
+
+        for (int x = roadX - half; x <= roadX + half; x++)
+        {
+            for (int y = roadY - half; y <= roadY + half; y++)
+            {
+                if (InMapRange(x, y))
+                {
+                    map[x, y] = road;
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// Crates a game object that picks a random height and chooses random color for building
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    void CreateBuilding(int x, int y)
+    {
+        Vector3 currentPosition = new Vector3(x, 0, y);
+        Vector3 centerPosition = new Vector3(width / 2f, 0, height / 2f);
+        float distance = Vector3.Distance(currentPosition, centerPosition);
+        float maxDistance = Vector3.Distance(Vector3.zero, centerPosition);
+        int buildHeight;
+
+        float heightChance = 1f - (distance / maxDistance);
+
+        if (heightChance > 0.7f)
+        {
+            buildHeight = Random.Range(9, 13);
+        }
+        else if (heightChance > 0.4f)
+        {
+            buildHeight = Random.Range(4, 9);
+        }
+        else
+        {
+            buildHeight = Random.Range(2, 5);
+        }
+
+        GameObject building = Instantiate(cube, transform);
+        building.transform.localPosition = new Vector3(x, buildHeight/2, y);
+        building.transform.localScale = new Vector3(1, buildHeight, 1);
+        building.name = "Building";
+
+        Renderer renderer = building.GetComponent<Renderer>();
+        renderer.material.color = Random.ColorHSV();
+    }
+
+    void CreateRoad(int x, int y)
+    {
+        GameObject road = Instantiate(cube, transform);
+        road.transform.localPosition = new Vector3(x, 0, y);
+        road.transform.localScale = Vector3.one;
+        road.name = "Road";
+    }
+
+    void DestroyCity()
+    {
+        if (transform.childCount > 0)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Destroy(transform.GetChild(i).gameObject);
+            }
+        }
+    }
+
+    bool InMapRange(int x, int y)
+    {
+        return x >= 0 && x < width && y >= 0 && y < height;
     }
 }
